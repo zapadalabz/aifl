81468c1c56de212267ca7f72b3f70df67f3a1b73
Zach Medendorp <zmedendorp@branksome.on.ca>
2023-11-04 15:00:07 -0400

    react

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useEffect} from "react";
+
+function App() {
+
+  useEffect(() => {
+    const sayHello = async () => {
+          const response = await fetch("/api/hello");
+          const body = await response.json();
+          console.log(body);
+        };
+        sayHello();
+    }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
+// Serve the React static files after build
+app.use(express.static("./client/build"));
 
 var port = process.env.PORT || 5000;
 
-app.get('/',(req,res) => res.send('Hello World'));
-
 app.listen (port, () => console.log("Listening on port " + port));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+
+app.get("/api/hello", (req, res) => {
+    res.send({ message: "Hello" });
+  });
