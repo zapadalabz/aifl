201c75e30e3cccbe27826dba3c0cf5263095ae23
Zach Medendorp <zmedendorp@branksome.on.ca>
2025-05-20 17:35:04 -0400

    default gpt-4o-mini

--- client/src/AppMain.js
+++ client/src/AppMain.js
@@ -46,7 +46,7 @@ function AppMain() {
 
   const [ user, setUser ] = useState(null);
   const [ chatHistory, setChatHistory] = useState([]);
-  const [selectedModel, setSelectedModel] = useState("o4-mini");
+  const [selectedModel, setSelectedModel] = useState("gpt-4o-mini");
 
   const [deviceType, setDeviceType] = useState(null);
 
